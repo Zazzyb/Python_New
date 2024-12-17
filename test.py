be01ca7515c097ca5867df18816344565551a9cd
Edi Ebibinitei <zazzy_b27@yahoo.com>
2024-12-17 20:07:11 +0100

    A Simple python test program

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 def drink():
+    print('Please enter your Full Name')
+    first_name = input('First Name : ')
+    surname = input('Surname : ') 
     print('Please input age')
     age = int(input(">> "))
     if age >= 18 and age <= 49:
-        print("Allowed to Party")
+        print(surname + " " + first_name + ", You're allowed to Party")
     elif age > 49:
-        print('Go back to your Family \n This is not for you')
+        print(surname + " " + first_name + ', Please go back to your Family \n This event is not for you')
     else:
-        print("Go back Home to your Daddy")
+        print(surname + " " + first_name + ", go back Home to your Daddy, Kid!!")
 
 drink()
